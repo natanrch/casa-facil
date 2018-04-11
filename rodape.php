469614ee0a66231b998c2a702edf625d2e28b550
André Luis <andreluis22.ap@gmail.com>
2018-04-11 15:26:00 -0300

    Alterado o tamanho dos icones redes sociais do footer

--- rodape.php
+++ rodape.php
@@ -1,15 +1,10 @@
 	<div class="foter">
-		
-		
 		<div class=" footer1  " style="flex: 1 6 10%">
-			
 
-			
 		</div>
 		<div class=" footer1 " style="flex: 1 6 30%">
 			<h2>Mais imóveis</h2>
 			<ul class="rodape">
-				
 				<a href="">	<li>Residenciais</li></a>
 				<a href=""><li>Apartamentos</li></a>
 				<a href=""><li>Casas</li></a>
@@ -18,42 +13,30 @@
 		</div>
 		<div  class=" footer1 " style="flex: 1 6 30%">
 			<h2>Informações </h2>
-
 			<ul class="rodape">
-
-				<a href="">	<li>Quem somos</li></a>
+				<a href=""><li>Quem somos</li></a>
 				<a href=""><li>Contato</li></a>
 				<a href=""><li>Alugue seu imóvel</li></a>
-				
 			</ul>
 		</div>
 
 		<div class=" footer1 " style="flex: 1 6 24%">
 			<h1>Entre em contato</h1>
 			<ol class="rodape_contatos">
-
-					<li>(96)991082902</li>
+				<li>(96)991082902</li>
 				<li>mateusbahia375@gmail.com</li>
-				
-
-				
 			</ol>
-			
-			<a class="btn btn-social-icon btn-facebook" style="background:  #3B5998">
-				<span class="fa fa-facebook"></span> 
+			<a class="btn btn-social-icon btn-facebook" style="background:  #3B5998; width: 40px; height: 38px; padding: 6px;font-size: 22px">
+				<span class="fa fa-facebook"></span>
 			</a>
-
-        <a class="btn btn-social-icon btn-twitter" style="background:   #55ACEE">
-				<span class="fa fa-twitter"></span> 
+        	<a class="btn btn-social-icon btn-twitter" style="background:   #55ACEE;width: 40px; height: 38px; padding: 6px;font-size: 22px">
+				<span class="fa fa-twitter"></span>
 			</a>
-			<a class="btn btn-social-icon btn-instagram" style="background:   #3F729B">
-				<span class="fa fa-instagram"></span> 
+			<a class="btn btn-social-icon btn-instagram" style="background:   #3F729B;width: 40px; height: 38px; padding: 6px;font-size: 22px">
+				<span class="fa fa-instagram"></span>
 			</a>
-			
 		</div>
-		
-		
-	</div>	
+	</div>
 	<footer class="forterrodape">
 		<div class="footer2 ">
 			<div  style="text-align: center;" >
@@ -66,6 +49,5 @@
 	<script src="js/popper.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<!-- <div></div> -->
-	
 </body>
 </html>
