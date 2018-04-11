e1eb32bc99f0617dceb84432cd373d4a0e902167
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2018-04-11 11:39:06 -0300

    Criação de rodapé único  em PHP

--- cadastro-cliente.php
+++ cadastro-cliente.php
@@ -139,17 +139,4 @@
 		
 		
 	</div>	
-	<footer class="forterrodape">
-		<div class="footer2 ">
-			<div  style="text-align: center;" >
-				<b style=" color: #fff;">Imobiliária casa facil© 2018 </b> &nbsp;&nbsp;- Todos os direitos reservados.
-			</div>
-		</div>
-	</footer>
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/popper.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<!-- <div></div> -->
-	
-</body>
-</html>
+<?php include 'rodape.php' ?>
