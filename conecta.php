354625aaecbb9194686d73d364a4ee216cdc7c4b
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2018-04-18 11:29:45 -0300

    Acrescentada explicação no arquivo conecta.php

--- conecta.php
+++ conecta.php
@@ -1,1 +1,3 @@
-$conexao = mysqli_connect("localhost", "root", "", "loja");
+//trocar o nome de usuário para o nome do usuário do mysql ou root. Quando for testar na máquina
+
+$conexao = mysqli_connect("localhost", "natan", "", "casa_facil");
