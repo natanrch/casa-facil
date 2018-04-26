12d6dbac4912e60a0641c3017ad77f7e9c8e6e18
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2018-04-26 12:29:27 -0300

    Construtor e getters da classe imovel

--- class/Imovel.php
+++ class/Imovel.php
@@ -3,7 +3,7 @@
 class Imovel {
 	protected $id;
 	protected $proprietario;
-	protected $enderco;
+	protected $endereco;
 	protected $bairro;
 	protected $area;
 	protected $tipo;
@@ -11,6 +11,63 @@ class Imovel {
 	protected $imagem;
 	protected $titulo;
 	protected $descricao;
+	//faltam demais atributos
+
+	public function __construct($proprietario, $endereco, $bairro, $area, $tipo, $valor, $imagem, $titulo, $descricao) {
+		$this->proprietario = $proprietario;
+		$this->endereco = $endereco;
+		$this->bairro = $bairro;
+		$this->area = $area;
+		$this->tipo = $tipo;
+		$this->valor = $valor;
+		$this->imagem = $imagem;
+		$this->titulo = $titulo;
+		$this->descricao = $descricao;
+	}
+
+	public function setId($id) {
+	 	$this->id = $id;
+	}
+
+	public function getId() {
+	 	return $this->id;
+	}
+
+	public function getProprietario() {
+	 	return $this->proprietario;
+	}
+
+	public function getEndereco() {
+		return $this->endereco;
+	}
+
+	public function getBairro() {
+		return $this->bairro;
+	}
+
+	public function getArea() {
+		return $this->area;
+	}
+
+	public function getTipo() {
+		return $this->tipo;
+	}
+
+	public function getValor() {
+		return $this->valor;
+	}
+
+	public function getImagem() {
+		return $this->imagem;
+	}
+
+	public function getTitulo() {
+		return $this->titulo;
+	}
+
+	public function getDescricao() {
+		return $this->descricao;
+	}
 }
 
 ?>
