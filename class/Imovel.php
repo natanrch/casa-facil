08891e741d6889fcd38cf625139f1749b623a729
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2018-04-26 12:13:22 -0300

    Criando classe imovel.php

---
+++ class/Imovel.php
@@ -0,0 +1,16 @@
+<?php
+
+class Imovel {
+	protected $id;
+	protected $proprietario;
+	protected $enderco;
+	protected $bairro;
+	protected $area;
+	protected $tipo;
+	protected $valor;
+	protected $imagem;
+	protected $titulo;
+	protected $descricao;
+}
+
+?>
