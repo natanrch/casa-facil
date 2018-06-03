ffee36f56ef048ea28bf185062446b056d013d91
mateusbahiadapenha <mateusbahia375@gmail.com>
2018-06-03 14:39:17 -0300

    adicionei a pagina de serviços ;

---
+++ servicos.php
@@ -0,0 +1,43 @@
+<?php
+include 'cria-cabecalho.php';
+criaCabecalho('Serviços', array( '-css/servicos.css'));
+?>
+
+<div class="container-fluide">
+	<div class="row">
+
+		<div class="col-md-12 servicos-home">
+			<h1>serviços</h1>
+		</div>
+	</div>
+</div>
+<div class="container-fluide">
+	<div class="row">
+
+		<div class="col-md-12 servicos-artigo1">
+			<p>Para que nosso cliente tenha sempre o melhor e que seu poder de escolha possa ser utilizado em diversas maneiras, a casa facil trabalha com uma vasta gama de serviços. Abaixo você pode conferir quais são todos os nossos serviços. Em caso de dúvida, entre em contato com a gente!</p>
+		</div>
+	</div>
+</div>
+<div class="container-fluide">
+	<div class="row">
+
+		<div class="col-md-4 servicos-artigo2">
+			<div class="servicos-opcao">
+				<h5>Aluguel de imóveis</h5>
+			</div>
+		</div>
+		<div class="col-md-4 servicos-artigo2">
+			<div class="servicos-opcao">
+				<h5>Pesquisa diversas</h5>
+			</div>
+		</div>
+		<div class="col-md-4 servicos-artigo2">
+			<div class="servicos-opcao">
+				<h5>localização de imóveis</h5>
+			</div>
+		</div>
+		
+	</div>
+</div>
+<?php include 'rodape.php' ?>
